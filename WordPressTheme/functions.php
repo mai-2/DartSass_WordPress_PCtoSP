2a431915802deb193e8dad45abed58c8fd4f78ae
mai-2 <71576918+mai-2@users.noreply.github.com>
2022-06-09 12:19:12 +0900

    Update functions.php

--- WordPressTheme/functions.php
+++ WordPressTheme/functions.php
@@ -35,9 +35,8 @@ add_action( 'after_setup_theme', 'my_setup' );
 function my_script_init()
 {
 
-	wp_enqueue_style('my', get_template_directory_uri() . '/assets/css/style.css', array(), '1.0.1', 'all' );
-	
-        wp_enqueue_script('my', get_template_directory_uri() . '/js/script.js', array( 'jquery' ), true);
+	wp_enqueue_style('my', get_template_directory_uri() . '/assets/css/style.css', array(), filemtime( get_theme_file_path( '/assets/css/style.css' )), 'all');
+	wp_enqueue_script('my', get_template_directory_uri() . '/assets/js/script.js', array( 'jquery' ), filemtime( get_theme_file_path( '/assets/js/script.js' )), true);
 
 }
 add_action('wp_enqueue_scripts', 'my_script_init');
